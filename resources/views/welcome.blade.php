c23281319c13791d938a8722c46e3d67236bd833
Thomas Trogen <thomas.trogen@gmail.com>
2017-10-14 14:30:55 +0200

    Lade till routes.php samt uppdaterade texten i welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Thomas Trogens New Laravel Page
+                    Thomas Trogens New GIT Laravel Page
                 </div>
 
                 <div class="links">
